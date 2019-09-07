c534b17085b896041c1909e884e4571710ecd68c
TomaszMarcinkowski86 <9454031marcinkowski@gmail.com>
2019-09-07 12:53:50 +0200

    create project

---
+++ src/main/java/pl/sda/housescape/HousescapeApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.housescape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousescapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousescapeApplication.class, args);
+    }
+
+}
