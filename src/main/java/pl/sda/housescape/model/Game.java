ea2aef2b4b7cba9dc41a944b2012d95fcad87643
TomaszMarcinkowski86 <9454031marcinkowski@gmail.com>
2019-09-07 15:34:50 +0200

    structure

---
+++ src/main/java/pl/sda/housescape/model/Game.java
@@ -0,0 +1,17 @@
+package pl.sda.housescape.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+public class Game {
+
+    private String name;
+    public Game(String name) {
+        this.name = name;
+
+
+    }
+}
