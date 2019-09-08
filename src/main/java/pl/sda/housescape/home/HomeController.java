32de5141d4ab8f5f9e85bba67f123a52e878b116
Rafal Gawlowski <rafciarz@gmail.com>
2019-09-08 10:21:24 +0200

    Add homeController and view

---
+++ src/main/java/pl/sda/housescape/home/HomeController.java
@@ -0,0 +1,16 @@
+package pl.sda.housescape.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @RequestMapping
+    public ModelAndView getHomePage(){
+        ModelAndView mnv = new ModelAndView("home");
+        return mnv;
+    }
+
+}
